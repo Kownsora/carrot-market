e6c08f6fa2faeab2a11d93dce0abca99af336762
Kownsora <cielo7824@gmail.com>
2023-07-10 03:00:27 +0900

    GET방식으로 데이터 조회
    입력한 데이터를 GET방식으로 데이터를 조회한다

--- frontend/write.js
+++ frontend/write.js
@@ -2,10 +2,13 @@ const form = document.getElementById("write-form");
 
 const handleSubmitForm = async (event) => {
   event.preventDefault();
+  const body = new FormData(form);
+  //세계시간 기준으로 보내줌
+  body.append("insertAt", new Date().getTime());
   try {
     const res = await fetch("/items", {
       method: "POST",
-      body: new FormData(form),
+      body: body,
     });
 
     const data = await res.json();
